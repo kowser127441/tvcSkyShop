3384f1c8fdb194eb522f07448aa33c84d7c26667
Nazmul Ahasan Maruf <marufaiub09@gmail.com>
2013-11-20 13:38:28 +0600

    add myaccount and information

---
+++ control_panel/logout.php
@@ -0,0 +1,11 @@
+<?php 
+ob_start(); 
+?>
+
+<?php
+
+session_start();
+session_destroy();
+header('Location: index.php');
+die();
+?>
--- control_panel/menu_right.php
+++ control_panel/menu_right.php
@@ -1,23 +1,16 @@
 <div class="nav-divider">&nbsp;</div>
 			<div class="showhide-account"><img src="images/shared/nav/nav_myaccount.gif" width="93" height="14" alt="" /></div>
 			<div class="nav-divider">&nbsp;</div>
-			<a href="" id="logout"><img src="images/shared/nav/nav_logout.gif" width="64" height="14" alt="" /></a>
+			<a href="logout.php" id="logout"><img src="images/shared/nav/nav_logout.gif" width="64" height="14" alt="" /></a>
 			<div class="clear">&nbsp;</div>
 		
 			<!--  start account-content -->	
 			<div class="account-content">
 			<div class="account-drop-inner">
-				<a href="" id="acc-settings">Settings</a>
+				
 				<div class="clear">&nbsp;</div>
 				<div class="acc-line">&nbsp;</div>
-				<a href="" id="acc-details">Personal details </a>
-				<div class="clear">&nbsp;</div>
-				<div class="acc-line">&nbsp;</div>
-				<a href="" id="acc-project">Project details</a>
-				<div class="clear">&nbsp;</div>
-				<div class="acc-line">&nbsp;</div>
-				<a href="" id="acc-inbox">Inbox</a>
-				<div class="clear">&nbsp;</div>
-				<div class="acc-line">&nbsp;</div>
-				<a href="" id="acc-stats">Statistics</a> 
+				<a href="myaccount.php" id="acc-details">Personal details </a>
+				
+				
 			</div>
