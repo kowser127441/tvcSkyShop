6a2b509557aaffbdaa475f4afb3c975727742644
Nazmul Ahasan Maruf <marufaiub09@gmail.com>
2013-11-21 13:57:18 +0600

    simple change

--- catagory_details.php
+++ catagory_details.php
@@ -1,3 +1,11 @@
+<?php
+error_reporting(0);
+$category_id=$_REQUEST['category_id'];
+$sub_category_id=$_REQUEST['sub_category_id'];
+
+?>
+    
+
 			<div id="left">
             	<div id="menu">
                 	<!--start menu-->
@@ -34,151 +42,65 @@
                 <!--</div><!--end pro_slider--><!--<div class="clear"></div>-->
                 
                 <div class="catagory">
-                	<h2 class="cata_name">Health and Harbal <span class="cata_all"><a href="catagory.php">View All <img src="images/catagory_arrow.png"></a></span></h2>
+               
+                <?php include("connection.php");
+
+             
+			 $cat_sql = mysql_query("SELECT category_name FROM category WHERE category_id = '$category_id'");
+			 while($cat_data= mysql_fetch_array($cat_sql))
+			 {
+				$category_name = $cat_data['category_name'];
+			 }
+
+				
+              
+
+			$sql = mysql_query("SELECT * FROM product WHERE category_id LIKE '%$category_id%' ");
+			while($data=mysql_fetch_array($sql))	
+			{
+				$product_id = $data['product_id'];
+				$name = $data['product_name'];
+				$details = $data['details'];
+				$terms = $data['terms'];
+				$price = $data['price'];
+				$size = $data['size'];
+				$discount = $data['discount'];
+				$discount_available = $data['discount_available'];
+				$discount_duration = $data['discount_duration'];
+				$discount_date= $data['discount_date'];
+				
+				$dif_pic=mysql_query("SELECT * FROM picture WHERE product_id='$product_id' AND _default='default'");
+					while($d_pic=mysql_fetch_array($dif_pic))
+					{
+						$dif_pic_url=$d_pic['picture_url'];
+					}
+			
+				
+				
+				
+			
+				
+				
+				
+				
+				?>
+                	<h2 class="cata_name"><?php echo $category_name; ?> <span class="cata_all"><a href="catagory.php?$category_id">View All <img src="images/catagory_arrow.png"></a></span></h2>
                     
                     <div class="product">
-                    	<a href="product.php" target="_blank"><img src="images/cata-img/csw_single_stripe_brown_by_civvy_street.jpg"></a>
-                        <h1><a href="product.php" target="_blank">CSW Single Stripe (Brown)</a></h1>
-                        <p class="old_price">Tk. 650.00</p>
+                    	<a href="product.php" target="_blank"><img src="<?php echo 'control_panel/'.$dif_pic_url; ?>"></a>
+                        <h1><a href="product.php" target="_blank"><?php echo $name ;?></a></h1>
+                        <p class="old_price"><?php echo $price?></p>
                         <p class="special_price">Tk. 425.00</p>
                     </div><!--end product-->
+                    <?php
+			      }
+			
+					?>
                     
-                    <div class="product">
-                    	<a href="product.php" target="_blank"><img src="images/cata-img/freshlook_contact_lenses_sterling_gray__1.jpg"></a>
-                        <h1><a href="product.php" target="_blank">Freshlook contact lenses (Sterling Gray)</a></h1>
-                        <p class="old_price">Tk. 650.00</p>
-                        <p class="special_price">Tk. 425.00</p>
-                    </div><!--end product-->
                     
-                   <div class="product">
-                    	<a href="product.php" target="_blank"><img src="images/cata-img/molfix_midi_eco_20x6_4-9kg_-6pktbox_baby_diaper.jpg"></a>
-                        <h1><a href="product.php" target="_blank">Molfix Midi Eco 20x6 (4-9kg)-Box Baby Diaper</a></h1>
-                        <p class="old_price">Tk. 650.00</p>
-                        <p class="special_price">Tk. 425.00</p>
-                    </div><!--end product-->
                     
-                    <div class="product">
-                    	<a href="product.php" target="_blank"><img src="images/cata-img/turquoise-black-set-_earrin.jpg"></a>
-                        <h1><a href="product.php" target="_blank">Turquoise-Black Set (Earring+Necklace)</a></h1>
-                        <p class="old_price">Tk. 650.00</p>
-                        <p class="special_price">Tk. 425.00</p>
-                    </div><!--end product-->
-                    <div class="clear"></div>
                     
-                    <div class="product">
-                    	<a href="product.php" target="_blank"><img src="images/cata-img/csw_single_stripe_brown_by_civvy_street.jpg"></a>
-                        <h1><a href="product.php" target="_blank">CSW Single Stripe (Brown)</a></h1>
-                        <p class="old_price">Tk. 650.00</p>
-                        <p class="special_price">Tk. 425.00</p>
-                    </div><!--end product-->
-                    
-                    <div class="product">
-                    	<a href="product.php" target="_blank"><img src="images/cata-img/freshlook_contact_lenses_sterling_gray__1.jpg"></a>
-                        <h1><a href="product.php" target="_blank">Freshlook contact lenses (Sterling Gray)</a></h1>
-                        <p class="old_price">Tk. 650.00</p>
-                        <p class="special_price">Tk. 425.00</p>
-                    </div><!--end product-->
-                    
-                   <div class="product">
-                    	<a href="product.php" target="_blank"><img src="images/cata-img/molfix_midi_eco_20x6_4-9kg_-6pktbox_baby_diaper.jpg"></a>
-                        <h1><a href="product.php" target="_blank">Molfix Midi Eco 20x6 (4-9kg)-Box Baby Diaper</a></h1>
-                        <p class="old_price">Tk. 650.00</p>
-                        <p class="special_price">Tk. 425.00</p>
-                    </div><!--end product-->
-                    
-                    <div class="product">
-                    	<a href="product.php" target="_blank"><img src="images/cata-img/turquoise-black-set-_earrin.jpg"></a>
-                        <h1><a href="product.php" target="_blank">Turquoise-Black Set (Earring+Necklace)</a></h1>
-                        <p class="old_price">Tk. 650.00</p>
-                        <p class="special_price">Tk. 425.00</p>
-                    </div><!--end product-->
-                    <div class="clear"></div>
-                    
-                    <div class="product">
-                    	<a href="product.php" target="_blank"><img src="images/cata-img/csw_single_stripe_brown_by_civvy_street.jpg"></a>
-                        <h1><a href="product.php" target="_blank">CSW Single Stripe (Brown)</a></h1>
-                        <p class="old_price">Tk. 650.00</p>
-                        <p class="special_price">Tk. 425.00</p>
-                    </div><!--end product-->
-                    
-                    <div class="product">
-                    	<a href="product.php" target="_blank"><img src="images/cata-img/freshlook_contact_lenses_sterling_gray__1.jpg"></a>
-                        <h1><a href="product.php" target="_blank">Freshlook contact lenses (Sterling Gray)</a></h1>
-                        <p class="old_price">Tk. 650.00</p>
-                        <p class="special_price">Tk. 425.00</p>
-                    </div><!--end product-->
-                    
-                   <div class="product">
-                    	<a href="product.php" target="_blank"><img src="images/cata-img/molfix_midi_eco_20x6_4-9kg_-6pktbox_baby_diaper.jpg"></a>
-                        <h1><a href="product.php" target="_blank">Molfix Midi Eco 20x6 (4-9kg)-Box Baby Diaper</a></h1>
-                        <p class="old_price">Tk. 650.00</p>
-                        <p class="special_price">Tk. 425.00</p>
-                    </div><!--end product-->
-                    
-                    <div class="product">
-                    	<a href="product.php" target="_blank"><img src="images/cata-img/turquoise-black-set-_earrin.jpg"></a>
-                        <h1><a href="product.php" target="_blank">Turquoise-Black Set (Earring+Necklace)</a></h1>
-                        <p class="old_price">Tk. 650.00</p>
-                        <p class="special_price">Tk. 425.00</p>
-                    </div><!--end product-->
-                    <div class="clear"></div>
-                    
-                    <div class="product">
-                    	<a href="product.php" target="_blank"><img src="images/cata-img/csw_single_stripe_brown_by_civvy_street.jpg"></a>
-                        <h1><a href="product.php" target="_blank">CSW Single Stripe (Brown)</a></h1>
-                        <p class="old_price">Tk. 650.00</p>
-                        <p class="special_price">Tk. 425.00</p>
-                    </div><!--end product-->
-                    
-                    <div class="product">
-                    	<a href="product.php" target="_blank"><img src="images/cata-img/freshlook_contact_lenses_sterling_gray__1.jpg"></a>
-                        <h1><a href="product.php" target="_blank">Freshlook contact lenses (Sterling Gray)</a></h1>
-                        <p class="old_price">Tk. 650.00</p>
-                        <p class="special_price">Tk. 425.00</p>
-                    </div><!--end product-->
-                    
-                   <div class="product">
-                    	<a href="product.php" target="_blank"><img src="images/cata-img/molfix_midi_eco_20x6_4-9kg_-6pktbox_baby_diaper.jpg"></a>
-                        <h1><a href="product.php" target="_blank">Molfix Midi Eco 20x6 (4-9kg)-Box Baby Diaper</a></h1>
-                        <p class="old_price">Tk. 650.00</p>
-                        <p class="special_price">Tk. 425.00</p>
-                    </div><!--end product-->
-                    
-                    <div class="product">
-                    	<a href="product.php" target="_blank"><img src="images/cata-img/turquoise-black-set-_earrin.jpg"></a>
-                        <h1><a href="product.php" target="_blank">Turquoise-Black Set (Earring+Necklace)</a></h1>
-                        <p class="old_price">Tk. 650.00</p>
-                        <p class="special_price">Tk. 425.00</p>
-                    </div><!--end product-->
-                    <div class="clear"></div>
-                    
-                    <div class="product">
-                    	<a href="product.php" target="_blank"><img src="images/cata-img/csw_single_stripe_brown_by_civvy_street.jpg"></a>
-                        <h1><a href="product.php" target="_blank">CSW Single Stripe (Brown)</a></h1>
-                        <p class="old_price">Tk. 650.00</p>
-                        <p class="special_price">Tk. 425.00</p>
-                    </div><!--end product-->
-                    
-                    <div class="product">
-                    	<a href="product.php" target="_blank"><img src="images/cata-img/freshlook_contact_lenses_sterling_gray__1.jpg"></a>
-                        <h1><a href="product.php" target="_blank">Freshlook contact lenses (Sterling Gray)</a></h1>
-                        <p class="old_price">Tk. 650.00</p>
-                        <p class="special_price">Tk. 425.00</p>
-                    </div><!--end product-->
-                    
-                   <div class="product">
-                    	<a href="product.php" target="_blank"><img src="images/cata-img/molfix_midi_eco_20x6_4-9kg_-6pktbox_baby_diaper.jpg"></a>
-                        <h1><a href="product.php" target="_blank">Molfix Midi Eco 20x6 (4-9kg)-Box Baby Diaper</a></h1>
-                        <p class="old_price">Tk. 650.00</p>
-                        <p class="special_price">Tk. 425.00</p>
-                    </div><!--end product-->
-                    
-                    <div class="product">
-                    	<a href="product.php" target="_blank"><img src="images/cata-img/turquoise-black-set-_earrin.jpg"></a>
-                        <h1><a href="product.php" target="_blank">Turquoise-Black Set (Earring+Necklace)</a></h1>
-                        <p class="old_price">Tk. 650.00</p>
-                        <p class="special_price">Tk. 425.00</p>
-                    </div><!--end product-->
+                
                     <div class="clear"></div>
                 </div><!--end catagory-->
                 <div class="clear"></div>
--- menu.php
+++ menu.php
@@ -1,3 +1,6 @@
+<?php
+include("connection.php");
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,104 +10,122 @@
 <body>
 <div id='cssmenu'>
 <ul>
-   <li class='active parent'><a href='catagory.php'><span>Health and Harbal</span></a>
-   	<ul>
-         <li class='has-sub'><a href='#'><span>Product 1</span></a>
-            <!--<ul>
-               <li><a href='#'><span>Sub Item</span></a></li>
-               <li class='last'><a href='#'><span>Sub Item</span></a></li>
-            </ul>-->
-         </li>
-         <li class='has-sub'><a href='#'><span>Product 2</span></a>
-            <!--<ul>
-               <li><a href='#'><span>Sub Item</span></a></li>
-               <li class='last'><a href='#'><span>Sub Item</span></a></li>
-            </ul>-->
-         </li>
-      </ul>
+   <li class='active parent'><a href='category.php?category_id=1'><span>Health and Harbal</span></a>
+    <ul>
+                            
+                            <?php
+							$man_query =mysql_query("SELECT * FROM sub_category WHERE category_id=1");
+							while($man=mysql_fetch_array($man_query))
+							{
+								$sub_category_id=$man['sub_category_id'];
+							?>
+						
+							<li><a href="category.php?category_id=1&sub_category_id=<?php echo $sub_category_id; ?>"><?php echo $man['sub_category_name']; ?></a></li>
+							
+							<?php
+							}
+							?>
+                            
+                            
+                        </ul>
    </li>
    
-   <li class='has-sub parent'><a href='catagory.php'><span>Beauty Care</span></a>
-      <ul>
-         <li class='has-sub'><a href='#'><span>Product 1</span></a>
-            <!--<ul>
-               <li><a href='#'><span>Sub Item</span></a></li>
-               <li class='last'><a href='#'><span>Sub Item</span></a></li>
-            </ul>-->
-         </li>
-         <li class='has-sub'><a href='#'><span>Product 2</span></a>
-            <!--<ul>
-               <li><a href='#'><span>Sub Item</span></a></li>
-               <li class='last'><a href='#'><span>Sub Item</span></a></li>
-            </ul>-->
-         </li>
-      </ul>
+   <li class='has-sub parent'><a href='category.php?category_id=2'><span>Beauty Care</span></a>
+       <ul>
+                            
+                            <?php
+							$woman_query =mysql_query("SELECT * FROM sub_category WHERE category_id=2");
+							while($woman=mysql_fetch_array($woman_query))
+							{
+								$sub_category_id=$woman['sub_category_id'];
+							?>
+						
+							<li><a href="category.php?category_id=2&sub_category_id=<?php echo $sub_category_id; ?>"><?php echo $woman['sub_category_name']; ?></a></li>
+							
+							<?php
+							}
+							?>
+                            
+                        </ul>
    </li>
    
-   <li class='has-sub parent'><a href='catagory.php'><span>Fitness and Sports</span></a>
+   <li class='has-sub parent'><a href='category.php?category_id=3'><span>Fitness and Sports</span></a>
       <ul>
-         <li class='has-sub'><a href='#'><span>Product 1</span></a>
-            <!--<ul>
-               <li><a href='#'><span>Sub Item</span></a></li>
-               <li class='last'><a href='#'><span>Sub Item</span></a></li>
-            </ul>-->
-         </li>
-         <li class='has-sub'><a href='#'><span>Product 2</span></a>
-            <!--<ul>
-               <li><a href='#'><span>Sub Item</span></a></li>
-               <li class='last'><a href='#'><span>Sub Item</span></a></li>
-            </ul>-->
-         </li>
-      </ul>
+                           
+                            <?php
+							$kids_query =mysql_query("SELECT * FROM sub_category WHERE category_id=3");
+							while($kids=mysql_fetch_array($kids_query))
+								{
+									$sub_category_id=$kids['sub_category_id'];
+								?>
+							
+								<li><a href="category.php?category_id=3&sub_category_id=<?php echo $sub_category_id; ?>"><?php echo $kids['sub_category_name']; ?></a></li>
+								
+								<?php
+								}
+							?>  
+                
+                           
+                        </ul>
    </li>
    
-    <li class='has-sub parent'><a href='catagory.php'><span>Home and Kitchen</span></a>
-      <ul>
-         <li class='has-sub'><a href='#'><span>Product 1</span></a>
-            <!--<ul>
-               <li><a href='#'><span>Sub Item</span></a></li>
-               <li class='last'><a href='#'><span>Sub Item</span></a></li>
-            </ul>-->
-         </li>
-         <li class='has-sub'><a href='#'><span>Product 2</span></a>
-            <!--<ul>
-               <li><a href='#'><span>Sub Item</span></a></li>
-               <li class='last'><a href='#'><span>Sub Item</span></a></li>
-            </ul>-->
-         </li>
-      </ul>
+    <li class='has-sub parent'><a href='category.php?category_id=4'><span>Home and Kitchen</span></a>
+       <ul>
+                            
+                             <?php
+							$fairness_query =mysql_query("SELECT * FROM sub_category WHERE category_id=4");
+							while($fairness=mysql_fetch_array($fairness_query))
+							{
+					$sub_category_id=$fairness['sub_category_id'];
+							?>
+						
+							<li><a href="category.php?category_id=4&sub_category_id=<?php echo $sub_category_id; ?>"><?php echo $fairness['sub_category_name']; ?></a></li>
+							
+							<?php
+							}
+							?> 
+                            
+                        </ul>
    </li>
    
-    <li class='has-sub parent'><a href='catagory.php'><span>Electronics</span></a>
-      <ul>
-         <li class='has-sub'><a href='#'><span>Product 1</span></a>
-            <!--<ul>
-               <li><a href='#'><span>Sub Item</span></a></li>
-               <li class='last'><a href='#'><span>Sub Item</span></a></li>
-            </ul>-->
-         </li>
-         <li class='has-sub'><a href='#'><span>Product 2</span></a>
-            <!--<ul>
-               <li><a href='#'><span>Sub Item</span></a></li>
-               <li class='last'><a href='#'><span>Sub Item</span></a></li>
-            </ul>-->
-         </li>
-      </ul>
+    <li class='has-sub parent'><a href='category.php?category_id=5'><span>Electronics</span></a>
+     <ul>
+                           
+                            <?php
+							$perfume_query =mysql_query("SELECT * FROM sub_category WHERE category_id=5");
+							while($perfume=mysql_fetch_array($perfume_query))
+							{
+								$sub_category_id=$perfume['sub_category_id'];
+							?>
+						
+							<li><a href="category.php?category_id=5&sub_category_id=<?php echo $sub_category_id; ?>"><?php echo $perfume['sub_category_name']; ?></a></li>
+							
+							<?php
+							}
+							?>
+                           
+                           
+                        </ul>
    </li>
     
-   <li class='last parent'><a href='catagory.php'><span>Others</span></a>
+   <li class='last parent'><a href='category.php?category_id=6'><span>Others</span></a>
    	<ul>
-         <li class='has-sub'><a href='#'><span>Product 1</span></a>
-            <!--<ul>
-               <li><a href='#'><span>Sub Item</span></a></li>
-               <li class='last'><a href='#'><span>Sub Item</span></a></li>
-            </ul>-->
-         </li>
-         <li class='has-sub'><a href='#'><span>Product 2</span></a>
-            <!--<ul>
-               <li><a href='#'><span>Sub Item</span></a></li>
-               <li class='last'><a href='#'><span>Sub Item</span></a></li>
-            </ul>-->
+                            
+                              <?php
+								$elec_query =mysql_query("SELECT * FROM sub_category WHERE category_id=6");
+								while($elec=mysql_fetch_array($elec_query))
+								{
+									$sub_category_id=$elec['sub_category_id'];
+								?>
+							
+								<li><a href="category.php?category_id=6&sub_category_id=<?php echo $sub_category_id; ?>"><?php echo $elec['sub_category_name']; ?></a></li>
+								
+								<?php
+								}
+								?>
+                            
+                            
+                        </ul>
          </li>
       </ul>
    </li>
